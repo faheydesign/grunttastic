7dce3a7e0e4feb79f76c553f27ab32aa12ee27de
John Fahey <faheydesign@gmail.com>
2014-08-16 11:26:12 -0700

    Adding support for clean, watch and browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,37 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        },
-      },
-      uses_defaults: ['*.js', 'scripts/**/*.js', ],
+    clean: ['dist/'],
+    browserify: {
+      'dist/js/bundle.js': ['js/main.js']
     },
+    jshint: {
+      // define the files to lint
+      files: ['Gruntfile.js', 'app/scripts/*.js', 'test/**/*.js', '*.js'],
+      // configure JSHint (documented at http://www.jshint.com/docs/)
+      options: {
+        // more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', ['jshint']);
+
+
+  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('build', ['jshint', 'clean', 'browserify', ]);
 };
